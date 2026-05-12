ec0056a03370370633af9377e1cc1cff0182eb62
Zach-H55 <zachhains2@gmail.com>
2026-05-12 18:29:08 -0400

    Completed Task 6 global Redux store setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
+import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
