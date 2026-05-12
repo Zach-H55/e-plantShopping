1541de2d1ce225aedfddb8553b50f471056d1850
Zach-H55 <zachhains2@gmail.com>
2026-05-12 18:16:39 -0400

    Completed Task 1 ProductList component layout

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,23 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
+const initialState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      const item = action.payload;
 
-    
+      const existingItem = state.items.find(
+        (product) => product.name === item.name
+      );
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({
+          ...item,
+          quantity: 1,
+        });
+      }
     },
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+export const { addItem } = cartSlice.actions;
 
-export default CartSlice.reducer;
+export default cartSlice.reducer;
